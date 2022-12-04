a981906bc8077b40823a52847a6d5fe96763e543
Robb Shecter <robb@public.law>
2022-12-03 23:56:58 -0700

    fix: filename

--- 2022/python_code/tests/day_1_test.py
+++ 2022/python_code/tests/day_1_test.py
@@ -2,7 +2,7 @@ import day_1
 
 
 def test_sample_data():
-    input = open("./tests/fixtures/day_1_sample_data").readlines()
+    input = open("./tests/fixtures/day_1_sample.txt").readlines()
 
     assert day_1.total_max_calories(input) == 24000
 
