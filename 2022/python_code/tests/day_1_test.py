f1c09b2409babb6b5a6e98bb790fc62c7d364edc
Robb Shecter <robb@public.law>
2022-12-03 23:56:31 -0700

    run with real data

--- 2022/python_code/tests/day_1_test.py
+++ 2022/python_code/tests/day_1_test.py
@@ -3,5 +3,11 @@ import day_1
 
 def test_sample_data():
     input = open("./tests/fixtures/day_1_sample_data").readlines()
-    
+
     assert day_1.total_max_calories(input) == 24000
+
+
+def test_real_data():
+    input = open("./tests/fixtures/day_1_input.txt").readlines()
+
+    assert day_1.total_max_calories(input) == 71481
