b1a5baa8c1ce8f2d0831c05c938689ddc578db0f
Robb Shecter <robb@public.law>
2022-12-04 00:19:21 -0700

    docs

--- 2022/elixir_code/lib/elixir_code.ex
+++ 2022/elixir_code/lib/elixir_code.ex
@@ -1,4 +1,11 @@
 defmodule Day_1 do
+  @moduledoc """
+  Day 1: Calorie Counting
+  """
+
+  @doc """
+  Find the Elf carrying the most Calories. How many total Calories is that Elf carrying?
+  """
   def total_max_calories(input) do
     0
   end
