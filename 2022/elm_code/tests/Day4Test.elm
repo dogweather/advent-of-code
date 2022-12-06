4a8e1e91ad1274f996b9eeee5ca27d9d1aefb958
Robb Shecter <robb@public.law>
2022-12-06 01:08:08 -0700

    passing test

--- 2022/elm_code/tests/Day4Test.elm
+++ 2022/elm_code/tests/Day4Test.elm
@@ -39,6 +39,11 @@ suite =
                     Day4.isContained { from = 2, to = 4 } { from = 1, to = 5 }
                         |> Expect.equal True
                 )
+            , test "basic true 3"
+                (\_ ->
+                    Day4.isContained { from = 1, to = 5 } { from = 2, to = 5 }
+                        |> Expect.equal True
+                )
             ]
 
         -- , describe
