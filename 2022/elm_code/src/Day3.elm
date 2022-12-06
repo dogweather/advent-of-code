67e14fa891bf77dfe1d7f01827edee9ec9673bfc
Robb Shecter <robb@public.law>
2022-12-05 19:58:52 -0700

    a lot more coding

--- 2022/elm_code/src/Day3.elm
+++ 2022/elm_code/src/Day3.elm
@@ -1,5 +1,8 @@
 module Day3 exposing (..)
 
+import Html exposing (input)
+import Set
+
 
 prioritiesSum : String -> Int
 prioritiesSum input =
@@ -15,11 +18,41 @@ toLines input =
     String.split "\n" input
 
 
-findMisplacedType : String -> String
-findMisplacedType _ =
-    "p"
+findMisplacedType : String -> Char
+findMisplacedType longString =
+    longString
+        |> splitInHalf
+        |> findCommonCharacter
 
 
-calculatePriority : String -> Int
+calculatePriority : Char -> Int
 calculatePriority _ =
     10
+
+
+splitInHalf : String -> List String
+splitInHalf input =
+    let
+        actualLen =
+            String.length input // 2
+    in
+    [ String.left actualLen input, String.right actualLen input ]
+
+
+findCommonCharacter : List String -> Char
+findCommonCharacter strings =
+    let
+        listOfSets =
+            strings
+                |> List.map String.toList
+                |> List.map Set.fromList
+    in
+    case listOfSets of
+        [ letters1, letters2 ] ->
+            Set.intersect letters1 letters2
+                |> Set.toList
+                |> List.head
+                |> Maybe.withDefault '-'
+
+        _ ->
+            '-'
